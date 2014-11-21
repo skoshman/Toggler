55d3bc2010f275bc05139584962dd2db87fd3e63
Sergei Koshman <sergei.koshman@altexsoft.com>
2014-11-21 20:06:47 +0200

    updated version

--- js/toggler.js
+++ js/toggler.js
@@ -1,15 +1,47 @@
 var toggler = {
 	init: function(){
-		var openers = document.querySelectorAll('.tgl-opener');
-
-		function Opener(content){
-			this.content = document.getElementById(content);
-			this.status = '';
+		var openers = document.querySelectorAll('[data-tgl-for]');
+		
+		function setOpenerContent(opener){
+			var contentNodes = document.querySelectorAll('[data-tgl-id]');
+			var contentId = opener.getAttribute('data-tgl-for');
+			
+			for (var i = 0; i < contentNodes.length; i++){
+				if (contentNodes[i].getAttribute('data-tgl-id') == contentId){
+					return contentNodes[i];
+				}
+			}
+		}
+		
+//		function Opener(){
+//			this.content = setOpenerContent(this);
+//			this.status = '';
+//		}
+		
+		for (var i = 0; i < openers.length; i++){
+			openers[i].content = setOpenerContent(openers[i]);
+			openers[i].status = openers[i].getAttribute('data-tgl-status');
 		}
 
-		for (var i = 0; i < openers.length; i++){
+		toggler.slide(openers[0].content);
+	},
+	
+	slide: function(box){
+		var initialHeight = box.offsetHeight;
+		box.style.height = initialHeight;
+		
+		while (initialHeight > 0){
+			box.style.height = initialHeight;
+			console.log(initialHeight, box.style.height);
+			initialHeight = initialHeight - 10;
 			
 		}
+
+//		for (var i = 0; i < 10; i++){
+//			console.log(i, initialHeight);
+//			box.style.height = initialHeight - 10;
+//			initialHeight = initialHeight - 10;
+//		}
 	}
 };
 window.addEventListener('load', function(){
