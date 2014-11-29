70281fd461d4f89458a8c9fa661a8c951f01457c
Sergei Koshman <sergei.koshman@altexsoft.com>
2014-11-29 23:37:25 +0200

    TogglerItem Class is updated

--- js/toggler.js
+++ js/toggler.js
@@ -1,40 +1,41 @@
 "use strict";
 
-//gets opener DOM element
-//returns DOM element is binded to this opener as a box to be toggled
-function bindContent(opener){
-	var key = opener.getAttribute('data-tgl-for');
-	var contentNodes = document.querySelectorAll('[data-tgl-id]');
-	var flag = false;
-	var i = 0;
-	
-	//meant only one element in the document could be recognized as content
-	//if there are more elements apper only the first one is binded
-	while (!flag){
-		if (contentNodes[i].getAttribute('data-tgl-id') === key){
-			flag = true;
-			return contentNodes[i];
-		}
-		i++;
-	}
-}
-
-//checks if content is shown or not
-//true - content is shown
-//false - content is hidden
-function getStatus(content){
-	if (content.className.indexOf('tgl-hide') !== -1){
-		return false;
-	}
-	else {
-		return true;
-	}
-}
-
 //define Class for Toggler instances
 function TogglerItem(opener){
+	//gets opener DOM element
+	//returns DOM element is binded to this opener as a box to be toggled
+	//function bindContent(opener){
+	var bindContent = function(opener){
+		var key = opener.getAttribute('data-tgl-for');
+		var contentNodes = document.querySelectorAll('[data-tgl-id]');
+		var flag = false;
+		var i = 0;
+
+		//meant only one element in the document could be recognized as content
+		//if there are more elements apper only the first one is binded
+		while (!flag){
+			if (contentNodes[i].getAttribute('data-tgl-id') === key){
+				flag = true;
+				return contentNodes[i];
+			}
+			i++;
+		}
+	};
+
+	//checks if content is shown or not
+	//true - content is shown
+	//false - content is hidden
+	var getStatus = function(content){
+		if (content.className.indexOf('tgl-hide') !== -1){
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
 	this.opener = opener;
-	this.content = bindContent(opener);
+	this.content = bindContent(this.opener);
 	this.status = getStatus(this.content);
 }
 
